764088fbe04db562e4b7795ba6f2664d5ec87124
Adarsh Gupta <adarshgupta139@gmail.com>
2016-12-18 17:16:55 +0530

    Added Source Code

---
+++ Assignment 1.3/src/Sess1_Assigmnt3.java
@@ -0,0 +1,20 @@
+
+public class Sess1_Assigmnt3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 20, b = 10;
+		System.out.println("Before Swapping: a: "+a+"\tb: "+b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("After  Swapping: a: "+a+"\tb: "+b);
+	}
+
+}
+/*
+******************************************************************************
+Before Swapping: a: 20	b: 10
+After  Swapping: a: 10	b: 20
+******************************************************************************
+*/
